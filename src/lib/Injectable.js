c170aad683ef69c1f7f1c1cb28a064902ab0a993
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-03-31 15:53:05 +0200

    feat: injectable classes now reference the same singleton injections

--- src/lib/Injectable.js
+++ src/lib/Injectable.js
@@ -23,29 +23,74 @@
  *     // bar.barProperty === foo.fooProperty - true
  *
  *
- *
- *
  * @param {Class} target
  * @param {Object} injections
  * @return {Class}
  */
 const Injectable = function (target, injections) {
 
+
     const handler = {
+
+        /**
+         * List of injections applied to target of this handler.
+         * Will be available with the field under which a particular injection was configured:
+         *
+         * @example
+         *   injections: {
+         *       prop: ServiceClass
+         *   }
+         *
+         *   // target.prop - [instanceof ServiceClass]
+         *
+         * @type {Object} injections
+         */
 
         construct (target, args) {
 
             /* eslint-disable-next-line */
-            console.log(`proxying constructor for ${target}`);
+            console.log(`calling proxyied constructor for ${target}`);
 
-            return new target(...args);
+            const injections = mapToSingletons(Object.entries({... (this.injections || {})}));
+
+            const injArgs = Object.fromEntries(injections);
+
+            // pass the injections as an additional operator
+            const inst = new target(...args, injArgs);
+
+            // to not accidentally replace any properties previously set with the constructor,
+            // we check if the properties already exist bevor applingg them
+            Object.entries(injArgs).forEach(([property, instance]) => {
+
+                if (inst[property] === undefined) {
+                    inst[property] = instance;
+                }
+            });
+
+            return inst;
         }
     };
 
-    target = new Proxy(target, handler);
+    const tHandler = {...handler, injections};
+    target = new Proxy(target, tHandler);
 
     return target;
 };
 
+const SINGLETONS = {};
+const mapToSingletons = function (mappings) {
+
+    return mappings.map(([prop, requiredClass]) => {
+
+        if (!SINGLETONS[requiredClass]) {
+            SINGLETONS[requiredClass] = new requiredClass;
+        }
+
+        return [prop, SINGLETONS[requiredClass]];
+
+    });
+
+
+};
 
 export {Injectable};
